280fffbb74e184696c6d1e4dc8b29a462ed59b08
Daniel Rau <drau88@cox.net>
2021-09-13 12:48:45 -0700

    Completed tasks 2 through 4. Beginning work on task 5.

--- index.js
+++ index.js
@@ -85,14 +85,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 function finalScore(inningcb, inningsToBePlayed){
-  let teamScores = {
+  const teamScores = {
     Home: inningcb(),
-    Away: inningcb()
+    Away: inningcb(),
   }
 
   for (let i = 0; i < inningsToBePlayed; i++){
-    teamScores.Home = teamScores.Home + inningcb;
-    teamScores.Away = teamScores.Away + inningcb;
+    teamScores.Home = teamScores.Home + (teamScores.Home + inningcb());
+    teamScores.Away = teamScores.Away + (teamScores.Away + inningcb());
   }
 
   return teamScores;
@@ -103,8 +103,12 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  const teamScores = {
+    Home: inningcb(),
+    Away: inningcb()
+  }
+  return teamScores;
 }
 
 
